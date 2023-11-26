d72f8cabdcfc72c8d35a5276f4f9c0a228aca7b8
candle <ragzouken@gmail.com>
2023-11-26 20:51:37 +0000

    context less annoying

--- src/AppContext.ts
+++ src/AppContext.ts
@@ -1,15 +1,15 @@
 import { createContext } from "react";
 import { Client, MediaItem } from "./client";
 
-export const AppContext = createContext({
-  danger: false,
-  client: new Client(),
-  password: null as string | null,
-  selected: null as MediaItem | null,
-  setSelected: (_: MediaItem | null) => {},
-  tryPassword: (_: string) => {},
-  refresh: () => {},
-  items: [] as MediaItem[],
-  updateItem: (_:MediaItem) => {},
-  removeItem: (_:MediaItem) => {},
-});
+export const AppContext = createContext<{
+  client: Client,
+  danger: boolean,
+  password: string | null,
+  selected: MediaItem | null,
+  setSelected: (item: MediaItem | null) => void,
+  tryPassword: (password: string) => void,
+  refresh: () => void,
+  items: MediaItem[],
+  updateItem: (item: MediaItem) => void,
+  removeItem: (item: MediaItem) => void;
+}>({} as any);
